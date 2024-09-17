0754320cacff493f323cbc4d4e0e6f004e729777
PeterBacily <peterbacily@gmail.com>
2024-09-17 19:50:57 +0200

    changes

--- todo.py
+++ todo.py
@@ -1,14 +1,3 @@
-# todo
-# check all plots apo snr 90 and snr 100
-# check all plot mercator
-# add plots to thesis file
-
-
-
-
-
-
-# done
-# vshift is correct and consistent between apo/merc now
-# - change apo linelist to include vshift
-# - redo files of both apo and mercator with new linelist
+#todo
+# - Make TVS images snr 90 APO+Mercator comparison
+# - Make Equavelent width images snr 90 +Mercator

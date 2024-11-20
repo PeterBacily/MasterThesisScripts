74b7b894d597e2d127ba33d4404abf2f1177cea2
PeterBacily <peterbacily@gmail.com>
2024-11-20 23:07:19 +0100

    changes

--- todo.py
+++ todo.py
@@ -2,3 +2,5 @@
 #
 # write start and end of each order into file, write code to find correct order for given wl
 # check if snr weirdness also exists in part of the spectra with no telluric lines.
+
+# try to take flat wl piece, normalize it, overplot for every individual spectrum and see if noise follows same shape across days.

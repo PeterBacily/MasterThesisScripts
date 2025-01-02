0906192fe595375de964cdc22ceb496df27e89da
PeterBacily <peterbacily@gmail.com>
2025-01-02 23:51:56 +0100

    changes

--- todo.py
+++ todo.py
@@ -1,5 +1,7 @@
 # todo
-#
+# test different rebin size files
+
+
 # write start and end of each order into file, write code to find correct order for given wl
 # check if snr weirdness also exists in part of the spectra with no telluric lines.
 

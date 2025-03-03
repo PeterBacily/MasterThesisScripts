1447fa8e4363c75f9b1945c5ab5dcce25c0851f4
PeterBacily <peterbacily@gmail.com>
2025-03-03 23:13:51 +0100

    changes

--- Specutils_test.py
+++ Specutils_test.py
@@ -28,6 +28,19 @@ linelist = ['line6562', 'line4713', 'line5411', 'line5801', 'line4541', 'line468
 apo_eshel_files= open_masterfiles.apo_demetra_orders(r"D:\peter\Master_Thesis\Datareduction\Converted_Data\demetra\with_orders\all_darks\rebin01\combined\high_snr\\")
 mercator_files = open_masterfiles.mercator(r'D:\peter\Master_Thesis\Datareduction\Converted_Data\mercator\ll_apo_vcor_2\\')
 
+
+
+def slice_and_norm(wl,flux,start,end,rebin=None):
+    slice_flux = flux[(wl > start) & (wl < end)]
+    slice_wl = wl[(wl > start) & (wl < end)]
+    slice_flux_norm = slice_flux / np.average(slice_flux)
+    if rebin == None:
+        return slice_wl,slice_flux_norm
+    else:
+        slice_wl_rebinned,slice_flux_rebinned = airmass.rebin2(slice_wl,slice_flux_norm,step=rebin)
+        return slice_wl_rebinned,slice_flux_rebinned
+
+
 def show_specutils_fit(file):
     line = linelist[0]
     list_of_orders = file.orders
@@ -121,13 +134,13 @@ def plot_normalization_test(apo_eshel_files,mercator_files):
     plt.show()
     plt.close()
 
-bd = [5170, 5210]
+bd = [5170, 5190]
 bd_ha = [6614.0, 6625.0]
 mercfile = mercator_files[1]
 m_wl = mercfile.wl_rebin
 m_flux = mercfile.flux_rebin
 m_wl_rebin,m_flux_rebin = airmass.rebin2(m_wl,m_flux)
-m_wl_deg,m_flux_deg = airmass.degrade_spectrum(m_wl,m_flux,pre_rebin=0.1,desired_snr=70)
+m_wl_deg,m_flux_deg = airmass.degrade_spectrum(m_wl,m_flux,pre_rebin=0.1,desired_snr=120)
 
 m_spec1d_deg = specutils.Spectrum1D(spectral_axis=m_wl_deg * AA, flux=m_flux_deg * ap_unit.Jy)
 print('specutils snr',analysis.snr_derived(m_spec1d_deg,specutils.SpectralRegion(bd_ha[0]*AA, bd_ha[1]*AA)))
@@ -136,13 +149,18 @@ m_snr_straight = airmass.snr_2(m_wl_deg,m_flux_deg,boundaries=bd,rebin=False,reb
 m_snr_ha = airmass.snr_2(m_wl_deg,m_flux_deg,boundaries=bd_ha,rebin=False,rebin_size=0.1,separate=False)
 
 apo_test_file = apo_eshel_files[0]
-apo_order_straight_line = airmass.find_order([5170, 5210],apo_test_file)
+apo_order_straight_line = airmass.find_order(bd,apo_test_file)
 
 #write code to measure snr in apo spectra
-
-
+apo_sl_wl = apo_order_straight_line.wl_rebin[2:]
+apo_sl_flux = apo_order_straight_line.flux_rebin[2:]/np.average(apo_order_straight_line.flux_rebin[2:])
+m_slice_wl, m_slice_flux = slice_and_norm(m_wl_deg,m_flux_deg,apo_sl_wl[0],apo_sl_wl[-1])
 print(m_snr_straight,m_snr_ha)
-plt.plot(m_wl_rebin,m_flux_rebin)
+plt.plot(apo_sl_wl,apo_sl_flux)
+plt.plot(m_slice_wl,m_slice_flux)
+plt.ylim(0.8,1.1)
+plt.axvline(bd[0])
+plt.axvline(bd[1])
 plt.show()
 plt.close()
 

7623a97977b65a2c8f457638e2092e7611e88f0a
PeterBacily <peterbacily@gmail.com>
2025-06-17 22:02:11 +0200

    changes

--- Remove_obs_and_run_datareduction.py
+++ Remove_obs_and_run_datareduction.py
@@ -67,7 +67,6 @@ def run_mdg(filelist,linelist,datagridfolder,vmin = -800,vmax = 800,k=1,selectio
     rb=0.5
     for i,line in enumerate(linelist):
         linekey = 'line' + str(int(line[k]))
-        print(i+1)
         data_grid = create_datafiles.make_data_grid(filelist,linekey, vmin,vmax,rebin_size=rb,selectionstring=selectionstring)
         savename = savefolder+'data_grid_'+line[0]+'_'+str(int(line[1]))+'rebin'+str(rb)+'_vlim'+str(vmin)+'_'+str(vmax)+'.txt'
         workfileresource = open(savename, 'wb')
@@ -110,14 +109,14 @@ def run_full_pipeline(filelist,linelist,datagrid_folder,LS_brick_folder,LS_plot_
     run_mlb(datagrid_folder,LS_brick_folder,frequencyarray=frequency_array)
     plot_LS_grid(LS_brick_folder,LS_plot_folder,v_min_ls=v_min_ls,v_max_ls=v_max_ls)
     make_sumplot(LS_brick_folder,Sumplot_folder)
-def feed_selection_into_pipeline(filelist, LS_brick_folder,LS_plot_folder,datagrid_folder,Sumplot_folder,plot_selectionstring):
+def feed_selection_into_pipeline(filelist, LS_brick_folder,LS_plot_folder,datagrid_folder,Sumplot_folder,plot_selectionstring,R=None,SNR_desired=None):
     linelist = open_masterfiles.open_linelist(str(converted_Data_folder) + r'\linelists\final_lls\linelist_no_hy.txt')
     # filelistpaths = glob.glob(r'D:\peter\Master_Thesis\Datareduction\Converted_Data\test\pipelinetest\masterfiles\\'+'*.txt')
     # filelist_nested = airmass.group_observations(open_masterfiles.mercator(manual_filelist=filelistpaths))
     # Wat hierboven staat als parameters meegeven
     filelist_flattened = flatten_list(filelist)
     filelist_sorted = sorted(filelist_flattened, key=lambda obj: obj.HJD)
-    run_full_pipeline(filelist_sorted,linelist,datagrid_folder,LS_brick_folder,LS_plot_folder,Sumplot_folder,R=10000,SNR_desired=150,selectionstring=plot_selectionstring)
+    run_full_pipeline(filelist_sorted,linelist,datagrid_folder,LS_brick_folder,LS_plot_folder,Sumplot_folder,R=R,SNR_desired=SNR_desired,selectionstring=plot_selectionstring)
 
 def generate_selection_strings(filelist,selection_method='Group',randomremoval_percent_removed = '0'):
     if selection_method=='Group':
@@ -136,17 +135,17 @@ def generate_selection_strings(filelist,selection_method='Group',randomremoval_p
         print('Selection method needs to be "Group" or "Random"')
     return selectionstring,foldernamestring
 
-def run_test_selection(selection,R=10000,snr_desired=20,sm='Group',rm=''):
+def run_selection(selection,R=10000,snr_desired=20,sm='Group',rm=''):
     plot_selectionstring,folder_selectionstring = generate_selection_strings(selection,selection_method=sm,randomremoval_percent_removed=rm)
-    databrick_base_folder = r'D:\peter\Master_Thesis\Datareduction\Converted_Data\test\pipelinetest\datagrid_folder'
-    lsbrick_base_folder = r'D:\peter\Master_Thesis\Datareduction\Converted_Data\test\pipelinetest\lsbrick'
-    ls_plot_base_folder = r'D:\peter\Master_Thesis\Datareduction\Converted_Data\test\pipelinetest\lsplot'
-    sum_ls_plot_base_folder = r'D:\peter\Master_Thesis\Datareduction\Converted_Data\test\pipelinetest\sumplot'
+    databrick_base_folder = r'D:\peter\Master_Thesis\Datareduction\Converted_Data\data_bricks\mercator_apolines_rebin05'
+    lsbrick_base_folder = r'D:\peter\Master_Thesis\Datareduction\Converted_Data\ls_bricks\mercator_apolines_rebin05'
+    ls_plot_base_folder = r'D:\peter\Master_Thesis\Datareduction\Plots\LS_periodogram\normal\mercator_apolines_rebin05'
+    sum_ls_plot_base_folder = r'D:\peter\Master_Thesis\Datareduction\Plots\LS_periodogram\summed\mercator_apolines_rebin05'
     databrickfolder = make_folderpath(databrick_base_folder,R=R,snr_desired=snr_desired,selectionstring=folder_selectionstring)
     lsbrickfolder = make_folderpath(lsbrick_base_folder,R=R,snr_desired=snr_desired,selectionstring=folder_selectionstring)
     lsplotfolder = make_folderpath(ls_plot_base_folder,R=R,snr_desired=snr_desired,selectionstring=folder_selectionstring)
     sumlsplotfolder = make_folderpath(sum_ls_plot_base_folder,R=R,snr_desired=snr_desired,selectionstring=folder_selectionstring)
-    feed_selection_into_pipeline(selection,LS_brick_folder=lsbrickfolder,datagrid_folder=databrickfolder,LS_plot_folder=lsplotfolder,Sumplot_folder=sumlsplotfolder,plot_selectionstring=plot_selectionstring)
+    feed_selection_into_pipeline(selection,LS_brick_folder=lsbrickfolder,datagrid_folder=databrickfolder,LS_plot_folder=lsplotfolder,Sumplot_folder=sumlsplotfolder,plot_selectionstring=plot_selectionstring, R=R,SNR_desired=snr_desired)
 # run_test_selection(open_masterfiles.mercator(r'D:\peter\Master_Thesis\Datareduction\Converted_Data\dataset_omar\\'))
 
 
@@ -154,17 +153,17 @@ def run_test_selection(selection,R=10000,snr_desired=20,sm='Group',rm=''):
 
 
 def run_pipeline_variations(observations, pipeline):
-    param_sets = [(None, None)] + [(10000, snr) for snr in [6, 8, 10, 15, 20, 30, 50]]
+    param_sets = [(None, None)] + [(10000, snr) for snr in [6, 8, 10, 15, 20, 30]]
 
     # --- Group-based removal ---
     grouped = airmass.group_observations(observations)
     total_groups = len(grouped)
 
-    for i in range(total_groups + 1):
+    for i in tqdm.tqdm(range(total_groups)):
         subset = [obs for group in grouped[i:] for obs in group]
 
         for R, snr in param_sets:
-            print(f"Group removal: {len(subset)} observations | R={R}, snr_desired={snr}")
+            # print(f"Group removal: {len(subset)} observations | R={R}, snr_desired={snr}")
             pipeline(subset, R=R, snr_desired=snr,sm='Group',rm='')
 
     # --- Percentage-based random removal ---
@@ -175,7 +174,7 @@ def run_pipeline_variations(observations, pipeline):
 
     num_steps = total // step_size
 
-    for step in range(1, num_steps + 1):  # skip step 0 (full dataset)
+    for step in tqdm.tqdm(range(1, num_steps + 1)):  # skip step 0 (full dataset)
         # Determine how many to remove this step
         to_remove = random.sample([obs for obs in observations if obs not in removed], step_size)
         removed.update(to_remove)
@@ -183,18 +182,18 @@ def run_pipeline_variations(observations, pipeline):
         subset = [obs for obs in observations if obs not in removed]
 
         for R, snr in param_sets:
-            print(f"Random removal: {len(subset)} observations | R={R}, snr_desired={snr}")
+            # print(f"Random removal: {len(subset)} observations | R={R}, snr_desired={snr}")
             pipeline(subset,R=R, snr_desired=snr,sm='Random',rm=f'{step*10:.0f}')
 
 def run_pipeline_variations_test(observations, pipeline):
-    param_sets = [(None, None)]
+    param_sets = [(10000, 20),(None,None)]
 
     # ------------------------
     # ✅ GROUP-BASED TESTS (simulate only 2 groups)
     # ------------------------
 
     grouped_all = airmass.group_observations(observations)
-    test_groups = grouped_all[-2:]  # Keep only the last 2 groups
+    test_groups = grouped_all[-1:]  # Keep only the last 2 groups
 
     for i in range(len(test_groups), 0, -1):  # 2 groups, then 1
         subset = [obs for group in test_groups[-i:] for obs in group]
@@ -208,7 +207,7 @@ def run_pipeline_variations_test(observations, pipeline):
     # ------------------------
 
     total = len(observations)
-    test_fractions = [0.20, 0.10]  # Keep 20% and 10%
+    test_fractions = [ 0.10]  # Keep 20% and 10%
 
     for frac in test_fractions:
         subset_size = max(1, int(total * frac))
@@ -218,4 +217,4 @@ def run_pipeline_variations_test(observations, pipeline):
             print(f"Random removal test: keeping {frac*100:.0f}% | {len(subset)} observations | R={R}, snr_desired={snr}")
             pipeline(subset, R=R, snr_desired=snr,sm='Random',rm=f'{frac*100:.0f}')
 
-run_pipeline_variations_test(open_masterfiles.mercator(r'D:\peter\Master_Thesis\Datareduction\Converted_Data\dataset_omar\\'),run_test_selection)
+run_pipeline_variations(open_masterfiles.mercator(r'D:\peter\Master_Thesis\Datareduction\Converted_Data\dataset_omar\\'),run_selection)
--- testfile.py
+++ testfile.py
@@ -11,9 +11,12 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 # os.environ['PYSYN_CDBS'] = 'C:\Users\Peter\Anaconda2\envs\p27\Lib\site-packages\pysynphot\data\cbds\grp\hst\cdbs'
 import scipy
-filelist = open_masterfiles.mercator(r'D:\peter\Master_Thesis\Datareduction\Converted_Data\dataset_omar\\')
-a=[1,2,3,4,5,6]
-print (a[:3])
+# filelist = open_masterfiles.mercator(r'D:\peter\Master_Thesis\Datareduction\Converted_Data\dataset_omar\\')
+file = r'D:\peter\Master_Thesis\Datareduction\Converted_Data\ls_bricks\mercator_apolines_rebin05\selection_202010-202010\degraded\R10000_snr100\He_I5875rebin0.5_ls_brick.txt'
+a = open(file, 'rb')
+line_period_info = pickle.load(a)
+a.close()
+print(line_period_info['paraminfo'])
 #
 # for item in textfilelist:
 #     if item not in tss_apo:
